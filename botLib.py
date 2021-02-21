1fdb94d814c491a0b442d8f376f73a1706d7728e
Ivan <affenmilchmann@gmail.com>
2021-02-21 21:57:33 +0300

    makeRequest func added

--- botLib.py
+++ botLib.py
@@ -1,6 +1,15 @@
-def mysum(a, b):
+from requests import request as sendreq
+from json import loads as loadjson
+
+def makeRequest(method, data = {}, headers = {}, files = [], token = "1664734318:AAH4E4V0uSm4PC1-kDFKtISbJtlLB4t71MM"):
     '''
-        Yay! You see the desctiption!\n
-        Returns summ of "a" and "b"
+    Makes a POST request https://api.telegram.org/bot + \<token> + / + \<method> \n
+    Returns a dict.\n
     '''
-    return a + b
+
+    url = "https://api.telegram.org/bot" + token + "/" + method
+
+    response = sendreq("POST", url, headers=headers, data=data, files=files)
+    resp_dict = loadjson(response.text.encode('utf8'))
+
+    return resp_dict
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from botLib import *
 
-# im testing docstrings (description of functions).
-# try to hover over 'mysum' and check if you see a destription of the func
-mysum(1, 2)
+#test example
+print(makeRequest("getMe"))
