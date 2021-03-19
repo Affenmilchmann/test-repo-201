769f366f30b835bf51bcf80af96b64da66844556
Ivan <affenmilchmann@gmail.com>
2021-03-19 19:43:31 +0300

    getUpdates

--- botLib.py
+++ botLib.py
@@ -19,3 +19,48 @@ def makeRequest(method, data = {}, headers = {}, files = [], token_ = token):
     resp_dict = loadjson(response.text.encode('utf8'))
 
     return resp_dict
+
+def getLastID():
+    with open("last_update_id.txt", "r") as f:
+        id_ = f.readline()
+        if id_ == "":
+            id_ = 0
+        return id_
+
+def setLastID(id_):
+    with open("last_update_id.txt", "w") as f:
+        f.write(str(id_ + 1))
+
+def getUpdates(relevant_ones = True):
+    '''
+    Returns dict with user messages.\n
+    Format:\n
+     { user1_id : [array_of_messages],\n
+    user2_id : ['Hello', 'Bye'] }
+    '''
+    #####################
+    if relevant_ones:
+        data = { "offset" : getLastID()}
+    else:
+        data= {}
+    #####################
+    
+    d_result = makeRequest("getUpdates", data=data)["result"]
+    if len(d_result) != 0:
+        setLastID(d_result[-1]["update_id"])
+
+    out_dict = {}
+
+    for upd in d_result:
+        if "message" in upd:
+            message = upd["message"]
+
+            user_id = message["from"]["id"]
+            text = message["text"]
+
+            if user_id in out_dict:
+                out_dict[user_id].append(text)
+            else:
+                out_dict[user_id] = [text]
+
+    return out_dict
