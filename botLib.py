bbb83c4e4daa10e48a4ea769b4316518ea65a5d6
Ivan <affenmilchmann@gmail.com>
2021-03-23 14:41:52 +0300

    user data managment

--- botLib.py
+++ botLib.py
@@ -1,9 +1,17 @@
 from requests import request as sendreq
-from json import loads as loadjson
+from json import loads, load, dump
+from os import remove as removeFile, walk
 
+##init
 token = ""
 with open("token.txt", "r") as f:
     token = f.read()
+
+USER_DATA_PATH = "user_data/"
+
+########################
+##### ASSIST FUNCS #####
+########################
 
 def makeRequest(method, data = {}, headers = {}, files = [], token_ = token):
     '''
@@ -16,7 +24,7 @@ def makeRequest(method, data = {}, headers = {}, files = [], token_ = token):
     #sending a request
     response = sendreq("POST", url, headers=headers, data=data, files=files)
     #getting response
-    resp_dict = loadjson(response.text.encode('utf8'))
+    resp_dict = loads(response.text.encode('utf8'))
 
     return resp_dict
 
@@ -30,6 +38,10 @@ def getLastID():
 def setLastID(id_):
     with open("last_update_id.txt", "w") as f:
         f.write(str(id_ + 1))
+
+######################
+##### MAIN FUNCS #####
+######################
 
 def getUpdates(relevant_ones = True):
     '''
@@ -71,7 +83,7 @@ def sendMessage(user_id, text):
     -Returns True on success\n
     -Returns dict with provided by telegram error code end description on fail
     '''
-    data = {'chat_id' : user_id * 1000,
+    data = {'chat_id' : user_id,
             'text' : text }
 
     result = makeRequest("sendMessage", data=data)
@@ -81,4 +93,96 @@ def sendMessage(user_id, text):
     else:
         return result
 
-print(sendMessage(421823263, "Wasup"))
+def setUserData(user_id, data):
+    '''
+    Sets 'data' for user with 'user_id'.\n
+    'data' is a dict with format:\n
+    { 'drug_name' : [array with time strings] }\n
+    Example: { 'fennibut' : ['10:30', '15:00', '21:30'], 'noshpa' : ['15:30'] }
+    '''
+
+    with open(USER_DATA_PATH + str(user_id) + ".json", "w") as f:
+        dump(data, f)
+
+def getUserData(user_id):
+    '''
+    Gets data by 'user_id'. To see format check 'setUserData()' description
+    '''
+    try:
+        with open(USER_DATA_PATH + str(user_id) + ".json") as f:
+            return load(f)
+
+    except IOError:
+        return False
+        
+def editDrug(user_id, drug_name, new_timetable):
+    '''
+    Edits one drug's timetable.\n
+    If there is no drug with 'drug_name' then new drug will be added\n
+    'new_timetable' is array. To see format check 'setUserData()' description
+    '''
+    user_data = getUserData(user_id)
+    if not user_data:
+        return False
+
+    user_data[drug_name] = new_timetable
+
+    setUserData(user_id, user_data)
+
+def delDrug(user_id, drug_name):
+    '''
+    Deletes one drug's timetable\n
+    Returns 'False' if there is no file for user or\n
+    there is no drug with 'drug_name' in file.
+    '''
+    user_data = getUserData(user_id)
+    if not user_data:
+        return False
+
+    if drug_name in user_data:
+        del user_data[drug_name]
+    else:
+        return False
+
+    setUserData(user_id, user_data)
+
+def delUserFile(user_id):
+    '''
+    Deletes all drugs for user with 'user_id'\n
+    (Actually it just deletes the file)\n
+    Returns 'False' if there is no file for user
+    '''
+    try:
+        removeFile(USER_DATA_PATH + str(user_id) + ".json")
+    except IOError:
+        return False
+
+def getAllUsersData():
+    '''
+    Returns dict with all user data.\n
+    Format: { 'user1_id' : { 'drug1_name' : [array_with_time_strings] } }\n
+    Example: just use it and see, there is folder with test users. I cant fit example here. Sorry
+    '''
+    user_data = {}
+
+    files_list = []
+    for (dirpath, dirnames, filenames) in walk(USER_DATA_PATH):
+        files_list.extend(filenames)
+        break
+    
+    for file_name in files_list:
+        user_id = file_name
+        if user_id.endswith(".json"):
+                user_id = user_id[:-5]
+
+        with open(USER_DATA_PATH + file_name, 'r') as f:
+            user_data[user_id] = load(f)
+
+    return user_data
+            
+
+user_id = 12346
+dt = { 'fennibt' : ['10:30', '15:00', '21:30'], 'noshpa' : ['15:30'] }
+setUserData(user_id, dt)
+
+print(getAllUsersData())
