6eabc04bf68fcae693eddcce0c731922d3533a9a
Daria <detyukdaria@gmail.com>
2021-05-04 23:18:38 +0300

    edit sch handler

--- scheduleHandler.py
+++ scheduleHandler.py
@@ -5,9 +5,8 @@ from datetime import time, timedelta
 
 def scheduleHandler(SCHEDULE_CHECK):
     result = getAllUsersData()
-    local_time = time.localtime()
     for user in result:
         for drug in result[user].keys():
             for timestamp in result[user][drug]:
-                if time.strptime(timestamp) <= time.now() <= (time.now() + timedelta(seconds=SCHEDULE_CHECK)):
+                if time.strptime(timestamp) <= time.localtime() <= (time.localtime() + timedelta(seconds=SCHEDULE_CHECK)):
                     sendMessage(user, 'Please take the pill ' + drug)
