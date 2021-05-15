4ada59984684a04889f4f69243e24a1fbcaa7c9b
Ivan Osorgin <beblade@mail.ru>
2021-05-15 11:50:31 +0300

    фыв

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from scheduleHandler import *
 
 bot_active = True
 MESSAGE_CHECK = 15
-SCHEDULE_CHECK = 120
+SCHEDULE_CHECK = 40
 time_passed_since_schedule_check = 0
 
 print("[INFO] Bot is acive. Settings are:")
